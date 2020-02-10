0558f9f59340ceb4b525502c96b30c53d7382f67
Avani Patel <anpatel36@yahoo.in>
2020-02-10 18:00:42 -0500

    Initial Commit

---
+++ Day4InheritanceAndProtocol/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day4InheritanceAndProtocol
+//
+//  Created by Avani Patel on 2/10/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
